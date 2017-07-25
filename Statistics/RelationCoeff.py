120eac85a0d0504cc8adfcfe6e9a720955b5bcf2
malei <gorillazhip@126.com>
2017-07-25 22:22:45 +0800

    statistics

--- Statistics/RelationCoeff.py
+++ Statistics/RelationCoeff.py
@@ -11,37 +11,61 @@ mpl.rcParams['axes.unicode_minus'] = False
 '''
 计算皮尔逊系数
 '''
-def PearsonCoeff():
+def PearsonCoeff(db):
     conn = pymysql.connect(host="127.0.0.1",user="root",passwd="root",charset="utf8")
     tuples=()
+    if db =="acfun":
+        sql="select playnum,bulletnum,commentnum,favoritenum from acfun_video where playnum >0;"
+    else:
+        sql = "select play,bullets,comment,favourites from bilibili_video_info where play >0;"
+    '''
+    计算B站皮尔逊系数
+    '''
     try:
-        conn.select_db("acfun")
+        conn.select_db(db)
         cur = conn.cursor()
-        sql= "select playnum,bulletnum from acfun_video where playnum >0;"
+
         cur.execute(sql)
         tuples = cur.fetchall()
     except Exception,e:
         print e
     finally:
-        conn.close()
-    playnum=[]
+        pass
+    playnum = []
     bulletnum =[]
+    commentnum = []
+    favnum = []
     for t in tuples:
         playnum.append(t[0])
         bulletnum.append(t[1])
+        commentnum.append(t[2])
+        favnum.append(t[3])
 
-    PlayAndBullet = np.array((playnum,bulletnum))
+    dataarray= np.array((playnum,bulletnum,commentnum,favnum))
     #计算皮尔逊系数
-    PandBCoeff = np.corrcoef(PlayAndBullet)
-    #计算协方差矩阵
-    PandBCoeff = np.cov(PlayAndBullet)
-    print PandBCoeff
-    #结论：视频点击率与弹幕数不成正比
+    Pearson = np.corrcoef(dataarray)
+
+    print db+"皮尔逊系数矩阵："
+    print Pearson
 
 '''
-绘制累计分布图
+acfun皮尔逊系数矩阵：
+[[  1.00000000e+00  -1.21448254e-04   8.18913017e-02   5.91210743e-02]
+ [ -1.21448254e-04   1.00000000e+00  -1.36057732e-04   3.27156997e-03]
+ [  8.18913017e-02  -1.36057732e-04   1.00000000e+00   3.18446019e-01]
+ [  5.91210743e-02   3.27156997e-03   3.18446019e-01   1.00000000e+00]]
+bilibili_whole皮尔逊系数矩阵：
+[[ 1.          0.42213669  0.41229066  0.75650158]
+ [ 0.42213669  1.          0.96434245  0.38595039]
+ [ 0.41229066  0.96434245  1.          0.37189231]
+ [ 0.75650158  0.38595039  0.37189231  1.        ]]
 '''
-def CDFPlot():
+
+'''
+play number per user累计分布图
+'''
+
+def PlaynumPerUserCDF():
     '''
     A 站数据
     '''
@@ -59,9 +83,10 @@ def CDFPlot():
         conn.close()
     Atotalplaynum=[]
     for t in tuples:
-        Atotalplaynum.append(math.log(t[0],10))
+        # Atotalplaynum.append(math.log(t[0],10))
+        Atotalplaynum.append(t[0])
     #数据排序
-        Atotalplaynum.sort()
+    Atotalplaynum.sort()
     #计算数据比例 calculate the proportional values of samples，递增+1计数
     p1 = 100. * np.arange(1,len(Atotalplaynum)+1) / (len(Atotalplaynum))
 
@@ -71,7 +96,7 @@ def CDFPlot():
     try:
         conn.select_db("bilibili_whole")
         cur = conn.cursor()
-        sql= "select playnum from uppers_detail;"
+        sql= "select playnum from upper_detail;"
         cur.execute(sql)
         tuples = cur.fetchall()
     except Exception,e:
@@ -81,9 +106,9 @@ def CDFPlot():
     Btotalplaynum=[]
     for t in tuples:
         if t[0]!=0:
-            Btotalplaynum.append(math.log(t[0],10))
+            Btotalplaynum.append(t[0])
     #数据排序
-        Btotalplaynum.sort()
+    Btotalplaynum.sort()
     #计算数据比例 calculate the proportional values of samples，递增+1计数
     p2 = 100. * np.arange(1,len(Btotalplaynum)+1) / (len(Btotalplaynum))
 
@@ -93,23 +118,110 @@ def CDFPlot():
     #创建一个空figure
     fig = figure()
     #一行二列的图像，选择第一个
-    ax1 = fig.add_subplot(1,2,1)
-    ax1.plot(Atotalplaynum,p1)
-    ax1.set_xlabel(u'播放量/每个用户(log10)')
+    ax1 = fig.add_subplot(1,1,1)
+    ax1.semilogx(Atotalplaynum, p1)  # x轴为对数坐标轴
+    ax1.plot(Atotalplaynum,p1,label="acfun")
     ax1.set_ylabel(u'累计百分比%')
-    plt.title(u' Acfun用户播放量累计分布图')
+
+    # # 一行二列的图像，选择第二个
+    ax2 = fig.add_subplot(1,1,1)
+    ax2.semilogx(Btotalplaynum, p2)  # x轴为对数坐标轴
+    ax2.plot(Btotalplaynum, p2,color='#008B8B',label="bili")
+    ax2.set_xlabel(u'单个用户视频播放量')
+    ax2.set_ylabel(u'累计百分比%')
+    plt.title(u' Acfun/Bilibili单个用户播放量累计分布图')
+
+    plt.legend()
+    plt.plot()
+    plt.savefig("PlaynumPerUserCDF.png")
+    show()
+
+
+'''
+用户视频数量-频率双对数图
+'''
+def VideonumPerUserDoubleLog():
+    '''
+    A 站数据
+    '''
+    conn = pymysql.connect(host="127.0.0.1",user="root",passwd="root",charset="utf8")
+    tuples=()
+    try:
+        conn.select_db("acfun")
+        cur = conn.cursor()
+        sql= "select videocount from upper_detail;"
+        cur.execute(sql)
+        tuples = cur.fetchall()
+    except Exception,e:
+        print e
+    finally:
+        conn.close()
+    Atotalplaynum=[]
+    for t in tuples:
+        Atotalplaynum.append(t[0])
+    #数据排序
+    Atotalplaynum.sort()
+    #计算数据所占比例,存储某个数字在list中出现个数的字典
+    p1 = []
+    for a in Atotalplaynum:
+        percentage= float(Atotalplaynum.count(a))/float(len(Atotalplaynum))
+        p1.append(percentage)
+    print p1
+
+    #B站数据
+    conn = pymysql.connect(host="127.0.0.1",user="root",passwd="root",charset="utf8")
+    tuples=()
+    try:
+        conn.select_db("bilibili_whole")
+        cur = conn.cursor()
+        sql= "select videocount from upper_detail;"
+        cur.execute(sql)
+        tuples = cur.fetchall()
+    except Exception,e:
+        print e
+    finally:
+        conn.close()
+    Btotalplaynum=[]
+    for t in tuples:
+        Btotalplaynum.append(t[0])
+    #数据排序
+    Btotalplaynum.sort()
+
+    #计算数据所占比例,存储某个数字在list中出现个数的字典
+    p2 = []
+    for a in Btotalplaynum:
+        percentage= float(Btotalplaynum.count(a))/float(len(Btotalplaynum))
+        p2.append(percentage)
+    print p2
+
+    # plot the sorted data:
+    #创建一个空figure
+    fig = figure()
+    #一行二列的图像，选择第一个
+    ax1 = fig.add_subplot(1,2,1)
+    ax1.set_xlabel(u'单个用户上传视频数')
+    ax1.set_ylabel(u'频率')
+    plt.title(u' Acfun用户视频数量-频率双对数图')
+    # ax1.semilogx(x, y)  # x轴为对数坐标轴
+    # ax1.semilogy(x, y)  # y轴为对数坐标轴
+    #
+    ax1.loglog(Atotalplaynum, p1, linewidth=0)  # 双对数坐标轴
+    ax1.scatter(Atotalplaynum, p1, s=10, color='black')
 
     # # 一行二列的图像，选择第二个
     ax2 = fig.add_subplot(1,2,2)
-    ax2.plot(Btotalplaynum, p2)
-    ax2.set_xlabel(u'播放量/每个用户(log10)')
-    ax2.set_ylabel(u'累计百分比%')
-    plt.title(u' Bilibili用户播放量累计分布图')
 
-    plt.plot()
-    show()
-    savefig("CDF.png")
+    ax2.set_xlabel(u'单个用户上传视频数')
+    ax2.set_ylabel(u'频率')
+    plt.title(u' Bilibili用户视频数量-频率双对数图')
 
+    ax2.loglog(Btotalplaynum, p2, linewidth=0)  # 双对数坐标轴
+    ax2.scatter(Btotalplaynum, p2, s=10, color='black')
+    plt.show()
 
-CDFPlot()
+#
+# PearsonCoeff("acfun")
+# PearsonCoeff("bilibili_whole")
+# VideonumPerUserDoubleLog()
+PlaynumPerUserCDF()
 
